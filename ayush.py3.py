d4fa5fc81075fa16391badb5b5967ba34c2feee7
ayushtiwariat1616-stack <ayushtiwariat1616@gmail.com>
2025-09-09 19:55:20 +0530

    Add files via upload

---
+++ ayush.py3.py
@@ -0,0 +1,8 @@
+#WAP to store 'costarica' in the given list where
+# country is 'germany' or 'spain'
+b=['japan','germany','spain','ecuador']
+m='costarica'
+for k in range(3):
+     if b[k]=='spain' or b[k]=='germany':
+          b[k]=m
+print(b)
